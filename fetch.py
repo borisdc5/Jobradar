8a50e7446b1765ac8a9cd26a9811b04e8c91d795
Candice Lenglart <candicelenglart@Candices-MacBook-Air.local>
2026-05-20 16:53:56 +0200

    Add Maten to ESN list

--- fetch.py
+++ fetch.py
@@ -140,7 +140,7 @@ ESNS = [
     '5 degrés','5 degres','consulting efficiency','obeya it','futurwork',
     'acatus','avnir engineering','sweet it','apya','nexoris','bk consulting',
     'frydom','seven','go&dev','la tribu',
-    'skills and affinity','skill now','serv\'it','servit',
+    'skills and affinity','skill now','serv\'it','servit','maten',
     'cgi','safran','thales','shape it','smile group','smile',
 ]
 
